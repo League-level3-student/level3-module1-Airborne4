356bfc8038d314fd6ce186231ab383fde43edfcd
League Student <league@iMac-56.attlocal.net>
2019-05-17 17:05:42 -0700

    vdzf
    vd

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -12,30 +12,32 @@ public class HangMan implements KeyListener {
 	JFrame j = new JFrame();
 	JPanel jp = new JPanel();
 	JLabel jl = new JLabel();
-	String guessedWord = "";
+	char[] guessedWord;
 	String word2guess = Utilities.readRandomLineFromFile("dictionary.txt");
+	int lives= 5;
 
 	public HangMan() {
 		j.add(jp);
 		jp.add(jl);
-		jl.addKeyListener(this);
+		j.addKeyListener(this);
 		j.setVisible(true);
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guessedWord = new char[word2guess.length()];
 		for (int i = 0; i < word2guess.length(); i++) {
-			guessedWord += "_";
+			guessedWord[i] = '_';
 
 		}
 		System.out.println(word2guess);
 		System.out.println(guessedWord);
+		jl.setText(new String(guessedWord));
 		j.pack();
 	}
 
 	public static void main(String[] args) {
-		new HangMan();
 		String ques = JOptionPane.showInputDialog(null,
 				"Let's play some Hangman! How many words do you want to guess?");
 		System.out.println(ques);
-
+		new HangMan();
 	}
 
 	@Override
@@ -43,17 +45,27 @@ public class HangMan implements KeyListener {
 		// TODO Auto-generated method stub
 
 	}
-
+/*
+ * 	else {
+				lives --;
+			}
+			make sure to add this outside of the for loop
+ */
 	@Override
 	public void keyPressed(KeyEvent e) {
+		System.out.println("key press");
 		char eu = e.getKeyChar();
 		for (int i = 0; i < word2guess.length(); i++) {
 			if (word2guess.charAt(i) == e.getKeyChar()) {
-			
+				guessedWord[i] = e.getKeyChar();
+				if (new String(guessedWord).equals(word2guess)) {
+					JOptionPane.showMessageDialog(null, "YOU WON!");
+				}
 			}
 		}
-		jl.setText(guessedWord);
-		j.pack();
+		jl.setText(new String(guessedWord));
+		System.out.println(lives);
+		
 
 	}
 
